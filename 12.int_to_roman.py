7bfc8f9ad3bc03d98ea2d3869fe80f54d8fad6a5
Yash Dilip Sanklecha <sanklechayash017@gmail.com>
2026-05-11 11:05:02 +0530

    Fix formatting of thousands list in intToRoman

--- 12.int_to_roman.py
+++ 12.int_to_roman.py
@@ -1,6 +1,6 @@
 class Solution:
     def intToRoman(self, num):
-        thousands = ["", "M", "MM", "MMM"]
+        thousands= ["", "M", "MM", "MMM"]
         hundreds = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
         tens = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
         ones = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
